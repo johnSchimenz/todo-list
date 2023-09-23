d8a0513665bc74026247a9aa97ea7acb76af639e
John Schimenz <john.schimenz@gmail.com>
2023-09-23 07:35:22 -0500

    Create todo item factory

---
+++ main.js
@@ -0,0 +1,7 @@
+const todoItemFactory = (title, dueDate, description) => {
+    title = title;
+    dueDate = dueDate;
+    description = description;
+    checkboxButton = false;
+    deleteButton = false;
+}
