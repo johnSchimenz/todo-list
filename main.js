9bedd8e2c221cccfeb04a8c0c181fcf5c89c7e71
John Schimenz <john.schimenz@gmail.com>
2023-09-25 21:18:43 -0500

    Project array, todo items added to object as an array, and projects can be clicked

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 // Create a project
 const ProjectFactory = (title) => {
     title = title;
+    openedProject = false;
     deleteButton = false;
-    return {title, deleteButton};
+    return {title, openedProject, deleteButton};
 }
 
 // Create a few example projects (will delete later)
 const groceries = ProjectFactory('Groceries');
-console.log(groceries);
 const chores = ProjectFactory('Chores');
-console.log(chores);
 const tripPacking = ProjectFactory('Packing for Trip');
-console.log(tripPacking);
+
+// Array of projects
+let projectsArray = [groceries, chores, tripPacking];
 
 // Display projects in the DOM
 const leftBottomContainer = document.querySelector('#left-bottom-container');
 
 const groceriesProject = document.createElement('div');
 groceriesProject.setAttribute('id', groceries.title);
+groceriesProject.setAttribute('class', 'project');
 groceriesProject.textContent = groceries.title;
 leftBottomContainer.appendChild(groceriesProject);
 const choresProject = document.createElement('div');
 choresProject.setAttribute('id', chores.title);
+choresProject.setAttribute('class', 'project');
 choresProject.textContent = chores.title;
 leftBottomContainer.appendChild(choresProject);
 const tripPackingProject = document.createElement('div');
 tripPackingProject.setAttribute('id', tripPacking.title);
+tripPackingProject.setAttribute('class', 'project');
 tripPackingProject.textContent = tripPacking.title;
 leftBottomContainer.appendChild(tripPackingProject);
 
@@ -43,20 +47,24 @@ const TodoItemFactory = (title, dueDate, description) => {
 const apples = TodoItemFactory('Apples', '1/1/2023', '1 bag');
 const bananas = TodoItemFactory('Bananas', '2/1/2023', '5');
 const groundBeef = TodoItemFactory('Ground Beef', '3/1/2023', '2 lb');
-console.log(apples);
-console.log(bananas);
-console.log(groundBeef);
 
 const makeBed = TodoItemFactory('Make bed', '1/1/2023', 'Clean sheets for the next week');
 const washDishes = TodoItemFactory('Wash dishes', '2/1/2023', 'Sinke should be clean');
 const vaccuum = TodoItemFactory('Vaccuum', '3/1/2023', 'All carpets and tile floors cleaned');
-console.log(makeBed);
-console.log(washDishes);
-console.log(vaccuum);
 
 const tShirts = TodoItemFactory('T-Shirts', '5/5/2023', '5');
 const sunblock = TodoItemFactory('Sunblock', '5/5/2023', '1');
 const camera = TodoItemFactory('Camera', '5/5/2023', '1');
-console.log(tShirts);
-console.log(sunblock);
-console.log(camera);
+
+// Add todo items to project objects
+groceries.todoArray = [apples, bananas, groundBeef];
+chores.todoArray = [makeBed, washDishes, vaccuum];
+tripPacking.todoArray = [tShirts, sunblock, camera];
+
+// Upon click, display todo items on right side
+const projects = document.querySelectorAll('.project');
+projects.forEach((project) => {
+    project.addEventListener('click', () => {
+        console.log('clicked');
+    })
+})
