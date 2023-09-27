c1bc2e0971ed80c0002ed36db7b390f83e42993c
John Schimenz <john.schimenz@gmail.com>
2023-09-26 23:25:14 -0500

    On click, shows DOM element, but need to tie DOM element to an object or array

--- main.js
+++ main.js
@@ -18,23 +18,23 @@ let projectsArray = [groceries, chores, tripPacking];
 const leftBottomContainer = document.querySelector('#left-bottom-container');
 
 const groceriesProject = document.createElement('div');
-groceriesProject.setAttribute('id', groceries.title);
+groceriesProject.setAttribute('id', groceries.title.toLowerCase());
 groceriesProject.setAttribute('class', 'project');
 groceriesProject.textContent = groceries.title;
 leftBottomContainer.appendChild(groceriesProject);
 const choresProject = document.createElement('div');
-choresProject.setAttribute('id', chores.title);
+choresProject.setAttribute('id', chores.title.toLowerCase());
 choresProject.setAttribute('class', 'project');
 choresProject.textContent = chores.title;
 leftBottomContainer.appendChild(choresProject);
 const tripPackingProject = document.createElement('div');
-tripPackingProject.setAttribute('id', tripPacking.title);
+tripPackingProject.setAttribute('id', 'packing-for-trip'); // Different from others since title has spaces
 tripPackingProject.setAttribute('class', 'project');
 tripPackingProject.textContent = tripPacking.title;
 leftBottomContainer.appendChild(tripPackingProject);
 
-// Create a todo item
-const TodoItemFactory = (title, dueDate, description) => {
+// Create a toDo item
+const toDoItemFactory = (title, dueDate, description) => {
     title = title;
     dueDate = dueDate;
     description = description;
@@ -43,28 +43,28 @@ const TodoItemFactory = (title, dueDate, description) => {
     return {title, dueDate, description, checkboxButton, deleteButton};
 }
 
-// Create some example todo items (will delete later)
-const apples = TodoItemFactory('Apples', '1/1/2023', '1 bag');
-const bananas = TodoItemFactory('Bananas', '2/1/2023', '5');
-const groundBeef = TodoItemFactory('Ground Beef', '3/1/2023', '2 lb');
+// Create some example toDo items (will delete later)
+const apples = toDoItemFactory('Apples', '1/1/2023', '1 bag');
+const bananas = toDoItemFactory('Bananas', '2/1/2023', '5');
+const groundBeef = toDoItemFactory('Ground Beef', '3/1/2023', '2 lb');
 
-const makeBed = TodoItemFactory('Make bed', '1/1/2023', 'Clean sheets for the next week');
-const washDishes = TodoItemFactory('Wash dishes', '2/1/2023', 'Sinke should be clean');
-const vaccuum = TodoItemFactory('Vaccuum', '3/1/2023', 'All carpets and tile floors cleaned');
+const makeBed = toDoItemFactory('Make bed', '1/1/2023', 'Clean sheets for the next week');
+const washDishes = toDoItemFactory('Wash dishes', '2/1/2023', 'Sink should be clean');
+const vaccuum = toDoItemFactory('Vaccuum', '3/1/2023', 'All carpets and tile floors cleaned');
 
-const tShirts = TodoItemFactory('T-Shirts', '5/5/2023', '5');
-const sunblock = TodoItemFactory('Sunblock', '5/5/2023', '1');
-const camera = TodoItemFactory('Camera', '5/5/2023', '1');
+const tShirts = toDoItemFactory('T-Shirts', '5/5/2023', '5');
+const sunblock = toDoItemFactory('Sunblock', '5/5/2023', '1');
+const camera = toDoItemFactory('Camera', '5/5/2023', '1');
 
-// Add todo items to project objects
-groceries.todoArray = [apples, bananas, groundBeef];
-chores.todoArray = [makeBed, washDishes, vaccuum];
-tripPacking.todoArray = [tShirts, sunblock, camera];
+// Add toDo items to project objects
+groceries.toDoArray = [apples, bananas, groundBeef];
+chores.toDoArray = [makeBed, washDishes, vaccuum];
+tripPacking.toDoArray = [tShirts, sunblock, camera];
 
-// Upon click, display todo items on right side
+// Upon click, display toDo items on right side
 const projects = document.querySelectorAll('.project');
 projects.forEach((project) => {
     project.addEventListener('click', () => {
-        console.log('clicked');
+        console.log(project);
     })
 })
